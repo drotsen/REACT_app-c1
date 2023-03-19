a5c8337f6e9ec2f0cf0f378ace67f51c76ec42e0
drotsen <marko.thoren@gmail.com>
2023-03-19 13:24:42 +0100

    third change

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ const age = 11;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {age>=18 ? <h1>OVERAGE</h1> : <h1>Under Age </h1>}
+
+      <User name="Bo" age={21} email= "Bt@email.com"/>
+      <User name="Lars" age={88} email= "LL@email.com"/>
+      <User name="Aven" age={44} email= "AV@email.com"/>
+      <Jobs salary={22900} position="VD" company= "ATAT"/>
+
     </div>
   );
 }
+
+
+const User = (props) =>{
+  return(
+    <div>
+    <h1>{props.name}</h1>
+    <h2>{props.email}</h2>
+    <h3>{props.age}</h3>
+    </div>
+  );
+};
+const Jobs = (props) =>{
+  return(
+    <div>
+    <h1>{props.salary}</h1>
+    <h2>{props.position}</h2>
+    <h3>{props.company}</h3>
+    </div>
+  );
+};
+
 
 export default App;
